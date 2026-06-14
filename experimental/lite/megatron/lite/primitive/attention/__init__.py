eea9ecb71cea3698e437c48ce72f8cc262e707fb
Yan Bai <bayan@nvidia.com>
2026-06-13 22:28:40 -0700

    Add Kimi K2 lite model support

---
+++ experimental/lite/megatron/lite/primitive/attention/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+"""Shared attention primitives."""
+
+from __future__ import annotations
+
+from megatron.lite.primitive.attention.mla import MultiLatentAttention
+
+__all__ = ["MultiLatentAttention"]
